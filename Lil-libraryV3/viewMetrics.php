7b35e8de6a1386190d6ec1e3af7a702b1d7aa68a
Gp32Prog66 <szojo7@hotmail.com>
2024-12-07 17:16:57 -0500

    Adding View Metrics Script

---
+++ Lil-libraryV3/viewMetrics.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+
+<htmL>
+    <head>
+    <h1>Book Metric Charts</h1>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script src=""></script>
+    </head>
+
+    <body>
+    <h1>Book Metric Charts</h1>
+    <canvas id="metricChart" width="400" height="200"></canvas>
+
+    </body>
+</htmL>
